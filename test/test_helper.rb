448e1e4575ad1420710eac0dbba55ff071a5774c
David Skoda <dskoda1@binghamton.edu>
2016-11-12 10:02:50 -0500

    add cov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+      add_filter 'app/jobs'
+      add_filter 'app/mailers'
+      add_filter 'app/channels'
 
+end
+Coveralls.wear!('rails')
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
